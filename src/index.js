961efa78974edb54e43b22b5dd19b8b18ab1e5e9
EvanilsonP <pevanilson99@gmail.com>
2023-05-11 14:22:42 -0300

    logging in into discord bot

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { Client, IntentBitsField } = require('discord.js');
 
 // intents are a set of permissions that your bot can use in order to get access to a set of events
+// client is our bot instance
 const client = new Client({
     intents: [ 
         IntentBitsField.Flags.Guilds,
@@ -9,4 +10,7 @@ const client = new Client({
         IntentBitsField.Flags.MessageContent
     ] 
 });
+
+// Log in into our discord bot
+client.login('MTEwNjI2MTE0NTg4NDU2OTY3MQ.GivtCB.15XJq0uvJfKZPSCZekji_f3ObhtnTzYj6mVZPU');
 
