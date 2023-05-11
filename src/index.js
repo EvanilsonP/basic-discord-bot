f49966bb578928a8b8ac9c676329a1a9e1bc9c65
EvanilsonP <pevanilson99@gmail.com>
2023-05-11 14:49:51 -0300

    interacting with the user

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-const { Client, IntentBitsField } = require('discord.js');
-
+const { Client, IntentsBitField } = require('discord.js');
 // intents are a set of permissions that your bot can use in order to get access to a set of events
 // client is our bot instance
 const client = new Client({
-    intents: [ 
-        IntentBitsField.Flags.Guilds,
-        IntentBitsField.Flags.GuildMembers,
-        IntentBitsField.Flags.GuildMessages,
-        IntentBitsField.Flags.MessageContent
-    ] 
+    intents: [
+      IntentsBitField.Flags.Guilds,
+      IntentsBitField.Flags.GuildMembers,
+      IntentsBitField.Flags.GuildMessages,
+      IntentsBitField.Flags.MessageContent,
+    ],
+  });
+
+// When our bot is online
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is online. ✔️ `);
+});
+
+client.on('messageCreate', (msg) => {
+// interacting with the user && preventing the bot from answering itself
+    if(msg.author.bot) {   
+        return;
+    };
+
+    if(msg.content === 'hello') {
+        msg.reply('Hey!')
+    }
 });
 
 // Log in into our discord bot
-client.login('MTEwNjI2MTE0NTg4NDU2OTY3MQ.GivtCB.15XJq0uvJfKZPSCZekji_f3ObhtnTzYj6mVZPU');
+client.login('MTEwNjI2MTE0NTg4NDU2OTY3MQ.Gr5zd0.Wa5cgFyZJOgSBXQw6d6okb1P3-z8gdP3LNufZg');
 
