44030aadf0b11396cf27bcd92ecc86e3ee798fc4
Evanilson P <109234914+EvanilsonP@users.noreply.github.com>
2023-05-13 22:25:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ client.on('messageCreate', (msg) => {
     };
 });
 
+// Sends a message to new members on the server
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
     if(!channel) return;
