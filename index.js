8a789c952b2c0f921babbe1fcfb2a1a13bdd0102
Evanilson P <109234914+EvanilsonP@users.noreply.github.com>
2023-05-14 19:19:31 -0300

    Creating a function to send a private message to a new member

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ client.on('ready', (c) => {
     console.log(`${c.user.tag} is online. ✔️ `);
 });
 
+// When a user types in one of the options
 client.on('messageCreate', (msg) => {
     // interacting with the user
     if (msg.content.toLocaleLowerCase() === 'hello') {
@@ -27,19 +28,29 @@ client.on('messageCreate', (msg) => {
     } else if (msg.content.toLocaleLowerCase() === 'hey') {
         msg.reply('Hey!');
     };
+
     // Preventing the bot from answering itself
     if(msg.author.bot) {   
         return;
     };
 });
 
-// Sends a message to new members on the server
+// Welcome message to new members
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
     if(!channel) return;
     channel.send(`Welcome to the server, ${member}`);
 });
 
+
+// Sends a private message to a new member on discord
+client.on('guildMemberAdd', (member) => {
+    const welcomeMessage = `Welcome, ${member}! We're glad you joined our server. Enjoy your stay!`;
+    member.send(welcomeMessage)
+    .then(() => console.log(`Sent a welcome message to ${member.user.tag}`))
+    .catch(console.error);
+});
+  
 // Log in into our discord bot
 client.login(process.env.TOKEN);
 
